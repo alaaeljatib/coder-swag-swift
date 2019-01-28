500563df47e88d0ef8ffa12f1a2b2bdd04beb05c
Alaa Eljatib <alaa.eljatib@gmail.com>
2019-01-27 23:22:15 -0500

    Add project files

---
+++ coder-swag/View/CategoryCell.swift
@@ -0,0 +1,14 @@
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    func updateViews(category: Category) {
+        categoryImage.image = UIImage(named: category.imageName)
+        categoryTitle.text = category.title
+    }
+
+}
